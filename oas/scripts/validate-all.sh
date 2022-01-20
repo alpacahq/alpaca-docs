32c348a05fba1a84a7503dac4ccce80a507a0dbb
Andrew Mcdonald <drew887121@gmail.com>
2022-01-20 16:05:54 -0500

    add initial attempt at oas GH action

---
+++ oas/scripts/validate-all.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#A simple script to loop over all our spec files and `npm run validate` on them
+
+set -e
+
+SCRIPT_DIR=$(dirname "$0")
+
+#import shared list of spec files
+source "$SCRIPT_DIR"/spec_files.sh
+
+#make sure we're in the same dir as our package json so our paths work as expected
+pushd "$SCRIPT_DIR"/../ >> /dev/null
+
+#re-enable e flag so we don't stop running after first failed lint
+set +e
+
+for dir in $SPEC_FILES
+do
+  npm run validate "$dir/openapi.yaml"
+done
+
+set -e
+
+#go back
+popd >> /dev/null
